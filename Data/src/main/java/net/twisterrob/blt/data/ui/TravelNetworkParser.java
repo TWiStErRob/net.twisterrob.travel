c2f6430e78d7dcb8d1cad78421417a0356b58a4b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2013-10-19 11:41:58 +0000

    [SVN] r747 Better London Travel in /Projects/Better London Travel/ [ADD] Handle list of files for one line for Tram merging

--- Data/src/main/java/net/twisterrob/blt/data/ui/TravelNetworkParser.java
+++ Data/src/main/java/net/twisterrob/blt/data/ui/TravelNetworkParser.java
@@ -13,48 +13,61 @@ import org.xml.sax.SAXException;
 
 public class TravelNetworkParser {
 	private static final String DATA_ROOT = "../temp/feed15/lultramdlrcablecarriver";
-	@SuppressWarnings("serial") private static final Map<Line, String> FILES = new HashMap<Line, String>() {
+	@SuppressWarnings("serial") private static final Map<Line, List<String>> FILES = new HashMap<Line, List<String>>() {
 		{
-			put(Line.Bakerloo, "tfl_1-BAK_-2-y05.xml");
-			put(Line.Central, "tfl_1-CEN_-670103-y05.xml");
-			put(Line.Circle, "tfl_1-CIR_-290103-y05.xml");
-			put(Line.District, "tfl_1-DIS_-1430113-y05.xml");
-			put(Line.HammersmithAndCity, "tfl_1-HAM_-290103-y05.xml");
-			put(Line.Jubilee, "tfl_1-JUB_-120140-y05.xml");
-			put(Line.Metropolitan, "tfl_1-MET_-3330101-y05.xml");
-			put(Line.Northern, "tfl_1-NTN_-530103-y05.xml");
-			put(Line.Piccadilly, "tfl_1-PIC_-500101-y05.xml");
-			put(Line.Victoria, "tfl_1-VIC_-350112-y05.xml");
-			put(Line.WaterlooAndCity, "tfl_1-WAC_-60102-y05.xml");
-			put(Line.DLR, "tfl_25-DLR_-6-y05.xml");
-			put(Line.EmiratesAirline, "tfl_71-CABd-1-y05.xml");
-			put(Line.Tram, "tfl_63-TRM1-1-y05.xml");
+			put(Line.Bakerloo, Arrays.asList("tfl_1-BAK_-2-y05.xml"));
+			put(Line.Central, Arrays.asList("tfl_1-CEN_-670103-y05.xml"));
+			put(Line.Circle, Arrays.asList("tfl_1-CIR_-290103-y05.xml"));
+			put(Line.District, Arrays.asList("tfl_1-DIS_-1430113-y05.xml"));
+			put(Line.HammersmithAndCity, Arrays.asList("tfl_1-HAM_-290103-y05.xml"));
+			put(Line.Jubilee, Arrays.asList("tfl_1-JUB_-120140-y05.xml"));
+			put(Line.Metropolitan, Arrays.asList("tfl_1-MET_-3330101-y05.xml"));
+			put(Line.Northern, Arrays.asList("tfl_1-NTN_-530103-y05.xml"));
+			put(Line.Piccadilly, Arrays.asList("tfl_1-PIC_-500101-y05.xml"));
+			put(Line.Victoria, Arrays.asList("tfl_1-VIC_-350112-y05.xml"));
+			put(Line.WaterlooAndCity, Arrays.asList("tfl_1-WAC_-60102-y05.xml"));
+			put(Line.DLR, Arrays.asList("tfl_25-DLR_-6-y05.xml"));
+			put(Line.EmiratesAirline, Arrays.asList("tfl_71-CABd-1-y05.xml"));
+			put(Line.Tram, Arrays.asList("tfl_63-TRM1-1-y05.xml", "tfl_63-TRM2-1-y05.xml", "tfl_63-TRM3-1-y05.xml",
+					"tfl_63-TRM4-1-y05.xml"));
 			//put(Line.Overground, "");
 		}
 	};
 	public static void main(String[] args) throws Throwable {
-		readAndDisplay(new File(DATA_ROOT, FILES.get(Line.Tram)), true);
-		testAll();
+		testAll(true, Arrays.asList(Line.Tram));
 	}
 
-	protected static void readAndDisplay(File file, boolean gui) throws IOException, SAXException,
+	protected static void readAndDisplay(boolean gui, File... files) throws IOException, SAXException,
 			FileNotFoundException {
 		JourneyPlannerTimetableHandler handler = new JourneyPlannerTimetableHandler();
-		try (FileInputStream stream = new FileInputStream(file)) {
-			JourneyPlannerTimetableFeed feed = handler.parse(stream);
-			System.out.printf("\033[1;35m%s\033[0m (\033[35m%s\033[0m)\n", //
-					feed.getLine(), feed.getOperator().getTradingName());
-			List<Route> routes = reconstruct(feed);
-			if (gui) {
-				new LineDisplay(feed.getLine(), routes, "Turnham Green").setVisible(true);
+		JourneyPlannerTimetableFeed feed = null;
+		for (File file: files) {
+			try (FileInputStream stream = new FileInputStream(file)) {
+				JourneyPlannerTimetableFeed readFeed = handler.parse(stream);
+				if (feed == null) {
+					feed = readFeed;
+				} else {
+					feed = feed.merge(readFeed);
+				}
 			}
+		}
+		System.out.printf("\033[1;35m%s\033[0m (\033[35m%s\033[0m)\n", feed.getLine(), feed.getOperator()
+				.getTradingName());
+		List<Route> routes = reconstruct(feed);
+		if (gui) {
+			new LineDisplay(feed.getLine(), routes, "Turnham Green").setVisible(true);
 		}
 	}
 
-	protected static void testAll() throws Throwable {
-		for (Line line: FILES.keySet()) {
-			File file = new File(DATA_ROOT, FILES.get(line));
-			readAndDisplay(file, false);
+	protected static void testAll(boolean display, Collection<Line> lines) throws Throwable {
+		for (Line line: lines) {
+			List<String> fileNames = FILES.get(line);
+			File[] files = new File[fileNames.size()];
+			int currentFile = 0;
+			for (String fileName: fileNames) {
+				files[currentFile++] = new File(DATA_ROOT, fileName);
+			}
+			readAndDisplay(display, files);
 		}
 	}
 
