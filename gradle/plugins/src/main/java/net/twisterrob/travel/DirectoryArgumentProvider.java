5ad428b17c939120a0040bfff013ba4257f01b76
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-12-20 21:19:32 +0000

    Simplify provider usage

--- gradle/plugins/src/main/java/net/twisterrob/travel/DirectoryArgumentProvider.java
+++ gradle/plugins/src/main/java/net/twisterrob/travel/DirectoryArgumentProvider.java
@@ -4,18 +4,16 @@ import java.util.List;
 
 import org.gradle.api.file.Directory;
 import org.gradle.api.provider.Provider;
-import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.process.CommandLineArgumentProvider;
 
-public class OutputDirectoryArgumentProvider implements CommandLineArgumentProvider {
+public abstract class DirectoryArgumentProvider implements CommandLineArgumentProvider {
 
 	private final Provider<Directory> directory;
 
-	public OutputDirectoryArgumentProvider(Provider<Directory> directory) {
+	public DirectoryArgumentProvider(Provider<Directory> directory) {
 		this.directory = directory;
 	}
 
-	@OutputDirectory
 	public Provider<Directory> getDirectory() {
 		return directory;
 	}
