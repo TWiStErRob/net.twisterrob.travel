a971e7506aae7fae8339d6f278b4cdcf4fed0390
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-12-23 12:30:43 +0000

    Fix idempotence issues

--- domain/status/src/commonMain/kotlin/net/twisterrob/travel/domain/london/status/Stacktrace.kt
+++ domain/status/src/commonMain/kotlin/net/twisterrob/travel/domain/london/status/Stacktrace.kt
@@ -2,4 +2,8 @@ package net.twisterrob.travel.domain.london.status
 
 class Stacktrace(
 	val stacktrace: String,
-)
+) {
+
+	override fun equals(other: Any?): Boolean =
+		other is Stacktrace && other.stacktrace == this.stacktrace
+}
--- domain/status/src/commonMain/kotlin/net/twisterrob/travel/domain/london/status/StatusContent.kt
+++ domain/status/src/commonMain/kotlin/net/twisterrob/travel/domain/london/status/StatusContent.kt
@@ -2,4 +2,8 @@ package net.twisterrob.travel.domain.london.status
 
 class StatusContent(
 	val content: String,
-)
+) {
+
+	override fun equals(other: Any?): Boolean =
+		other is StatusContent && other.content == this.content
+}
