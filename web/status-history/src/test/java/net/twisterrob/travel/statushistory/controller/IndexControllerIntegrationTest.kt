e1994b7149f4f88bc064a9837fd52d079953451c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-12-29 13:36:19 +0000

    Use @MicronautTest

--- web/status-history/src/test/java/net/twisterrob/travel/statushistory/controller/IndexControllerIntegrationTest.kt
+++ web/status-history/src/test/java/net/twisterrob/travel/statushistory/controller/IndexControllerIntegrationTest.kt
@@ -1,20 +1,23 @@
 package net.twisterrob.travel.statushistory.controller
 
-import io.micronaut.context.ApplicationContext
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
-import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import net.twisterrob.travel.statushistory.test.HtmlValidator.assertValidHtml
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.containsString
-import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
-// TODO use @MicronautTest in JUnit 5.
+@MicronautTest
 class IndexControllerIntegrationTest {
+
+	@Inject
+	@Client("/")
+	lateinit var client: HttpClient
 
 	@Test fun testIndex() {
 		val request: HttpRequest<Unit> = HttpRequest.GET("/")
@@ -34,30 +37,5 @@ class IndexControllerIntegrationTest {
 		assertNotNull(body)
 		val expected = IndexController::class.java.getResourceAsStream("/public/favicon.ico")?.use { it.readBytes() }
 		assertArrayEquals(expected, body)
-	}
-
-	companion object {
-
-		private lateinit var server: EmbeddedServer
-		private lateinit var client: HttpClient
-
-		@JvmStatic
-		@BeforeAll fun setupServer() {
-			server = ApplicationContext
-				.run(EmbeddedServer::class.java)
-			client = server
-				.applicationContext
-				.createBean(HttpClient::class.java, server.url)
-		}
-
-		@JvmStatic
-		@AfterAll fun stopServer() {
-			if (this::server.isInitialized) {
-				server.stop()
-			}
-			if (this::client.isInitialized) {
-				client.stop()
-			}
-		}
 	}
 }
