34ffbc16e233648fb577d9fcc03025d8d52a7441
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-04 18:36:10 +0100

    Reduce chances of creating duplicates in the same session

--- web/status-history/src/main/java/net/twisterrob/travel/statushistory/infrastructure/github/CreateGitHubIssueInteractor.kt
+++ web/status-history/src/main/java/net/twisterrob/travel/statushistory/infrastructure/github/CreateGitHubIssueInteractor.kt
@@ -6,6 +6,7 @@ import net.twisterrob.travel.statushistory.infrastructure.github.contract.Github
 import net.twisterrob.travel.statushistory.infrastructure.github.contract.GithubCreateIssueRequest
 import net.twisterrob.travel.statushistory.infrastructure.github.contract.GithubIssue
 import org.slf4j.LoggerFactory
+import java.util.concurrent.ConcurrentHashMap
 
 private val LOG = LoggerFactory.getLogger(CreateGitHubIssueInteractor::class.java)
 
@@ -16,12 +17,21 @@ private val LOG = LoggerFactory.getLogger(CreateGitHubIssueInteractor::class.jav
 class CreateGitHubIssueInteractor @Inject constructor(
 	private val github: GithubApiClient,
 ) : CreateGitHubIssueUseCase {
+
+	/**
+	 * Keep state to reduce the chances of creating the same issue multiple times.
+	 * For some reason the search API does not return the "just created" issues.
+	 */
+	private val alreadyCreated: MutableSet<String> = ConcurrentHashMap.newKeySet()
+
 	override fun ensureIssue(title: String, body: String) {
+		if (title in alreadyCreated) return
 		val issues = github.issuesWithTitle(title)
 			?: error("No response from GitHub, infrastructure error. Enable TRACE logging for HTTP client for details.")
 		if (issues.total_count == 0) { // TODO: implementation detail, move to a Repository.
 			github.createIssue(GithubCreateIssueRequest(title, body))
 				?: error("No response from GitHub, infrastructure error. Enable TRACE logging for HTTP client for details.")
+			alreadyCreated.add(title)
 		} else {
 			val issueLinks = issues.items.map(GithubIssue::html_url).joinToString(separator = "\n")
 			LOG.warn("Issue with title \"${title}\" already exists, not creating\n${body}\nExisting issue(s):\n${issueLinks}")
--- web/status-history/src/test/java/net/twisterrob/travel/statushistory/infrastructure/github/CreateGitHubIssueInteractorUnitTest.kt
+++ web/status-history/src/test/java/net/twisterrob/travel/statushistory/infrastructure/github/CreateGitHubIssueInteractorUnitTest.kt
@@ -11,7 +11,7 @@ import org.mockito.Mockito.verifyNoMoreInteractions
 import org.mockito.kotlin.argThat
 import org.mockito.kotlin.whenever
 
-class CreateGitHubIssueUnitTest {
+class CreateGitHubIssueInteractorUnitTest {
 	private val mockClient: GithubApiClient = mock()
 	private val subject = CreateGitHubIssueInteractor(mockClient)
 
@@ -40,6 +40,26 @@ class CreateGitHubIssueUnitTest {
 
 		subject.ensureIssue("Hello", "World")
 
+		verify(mockClient).issuesWithTitle("Hello")
+		verifyNoMoreInteractions(mockClient)
+	}
+
+	@Test fun `issue is not created when it was already created`() {
+		val searchResponse = GithubSearchIssuesResponse(0, true, emptyList())
+		whenever(mockClient.issuesWithTitle("Hello"))
+			.thenReturn(searchResponse)
+		val createRequest = {
+			argThat<GithubCreateIssueRequest> { title == "Hello" && body == "World" }
+		}
+		val createResponse = GithubIssue(1, "http://example.com/issue", "Hello")
+		whenever(mockClient.createIssue(createRequest()))
+			.thenReturn(createResponse)
+
+		subject.ensureIssue("Hello", "World")
+		subject.ensureIssue("Hello", "World")
+		subject.ensureIssue("Hello", "World")
+
+		verify(mockClient).createIssue(createRequest())
 		verify(mockClient).issuesWithTitle("Hello")
 		verifyNoMoreInteractions(mockClient)
 	}
