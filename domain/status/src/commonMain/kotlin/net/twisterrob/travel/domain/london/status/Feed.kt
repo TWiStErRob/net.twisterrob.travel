cf77215c3eb83f7426a4365cefff97bd97c7dca9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-11-05 01:36:42 +0000

    Add missing feed to fix HTTP 400 on cron

--- domain/status/src/commonMain/kotlin/net/twisterrob/travel/domain/london/status/Feed.kt
+++ domain/status/src/commonMain/kotlin/net/twisterrob/travel/domain/london/status/Feed.kt
@@ -3,4 +3,5 @@ package net.twisterrob.travel.domain.london.status
 enum class Feed {
 	TubeDepartureBoardsLineStatus,
 	TubeDepartureBoardsLineStatusIncidents,
+	TubeDepartureBoardsStationStatusIncidents,
 }
